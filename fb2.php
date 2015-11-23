941752c7a097e7a9c8062478011e8d3cd5791ee5
Max <2ovob4ehko@ukr.net>
2015-11-23 17:45:58 +0200

    Додав визначення всіх параметрів книги

--- fb2.php
+++ fb2.php
@@ -5,15 +5,36 @@ class Fb2 {
   private $bookAuthor;
   private $bookGenre;
   private $bookAnnotation;
-  private $bookCoverPage;
+  private $bookCoverImage;
   private $bookLang;
   private $bookYear;
   private $publishInfo;
   private $firstTitle;
-  private $sections;
+  private $sections=[];
 
-  function __construct() {
-
+  function __construct($url) {
+    $xml=simplexml_load_file($url);
+    if($xml===false){
+      return false;
+    }else{
+      $this->bookFile=$xml;
+      $t_info=$this->bookFile->description->{'title-info'};
+      $this->bookTitle=(string) $t_info->{'book-title'};
+      $this->bookAuthor=$t_info->author->{'first-name'}.' '.$t_info->author->{'last-name'};
+      $this->bookGenre=(string) $t_info->genre;
+      $this->bookAnnotation=$t_info->annotation->asXML();
+      $this->bookYear=(string) $t_info->date;
+      $this->bookLang=(string) $t_info->lang;
+      $this->publishInfo=$this->bookFile->description->{'publish-info'}->asXML();
+      foreach ($this->bookFile->body->title->children() as $child){
+        $this->firstTitle.=$child->asXML();
+      }
+      for($i=1;$i<count($this->bookFile->body->children());$i++){
+        array_push($this->sections,$this->bookFile->body->children()[$i]->asXML());
+      }
+      $this->bookCoverImage='data:image/jpeg;base64,'.$this->bookFile->binary;
+      return true;
+    }
   }
   function __toString() {
     return (string) $this->bookAuthor.' '.$this->bookTitle.' ('.$this->bookGenre.') - '.$this->bookYear;
@@ -27,18 +48,34 @@ class Fb2 {
       return true;
     }
   }
-  public function getInfo(){
-    if(!isset($this->bookFile)){
-      throw new Exception('Book file is not set');
-    }else{
-      $info=$this->bookFile->description->{'title-info'};
-      $this->bookTitle=(string) $info->{'book-title'};
-      $this->bookAuthor=$info->author->{'first-name'}.' '.$info->author->{'last-name'};
-      return (object) array(
-        'title'=>$this->bookTitle,
-        'author'=>$this->bookAuthor
-      );
-    }
+  public function getBookInfo(){
+    return (object) array(
+      'title'=>$this->bookTitle,
+      'author'=>$this->bookAuthor,
+      'genre'=>$this->genreToString($this->bookGenre),
+      'annotation'=>$this->bookAnnotation,
+      'year'=>$this->bookYear,
+      'lang'=>$this->bookLang
+    );
+  }
+  public function getPublishInfo(){
+    return $this->publishInfo;
+  }
+  public function getFirstTitle(){
+    return $this->firstTitle;
+  }
+  public function getSections(){
+    return $this->sections;
+  }
+  public function getCoverImage(){
+    return $this->bookCoverImage;
+  }
+  private function genreToString($name){
+    $st=array(
+      'sf_cyberpunk'=>'кіберпанк фантастика',
+      'sf_social'=>'соціальна фантастика'
+    );
+    return $st[$name];
   }
 }
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?
 include ('fb2.php');
-$book= new Fb2();
-$book->setBookFile('Арена.fb2');
-try {
-  $bookInfo=$book->getInfo();
-} catch(Exception $e){
-  echo $e->getMessage();
-}
+$book= new Fb2('Продавец.времени.fb2'); //Арена.fb2 Продавец.времени.fb2
+$bookInfo=$book->getBookInfo();
+$publishInfo=$book->getPublishInfo();
+$firstTitle=$book->getFirstTitle();
+$sections=$book->getSections();
+$coverImage=$book->getCoverImage();
 ?>
 <html>
   <head>
     <title>My FB2Reader Example</title>
     <meta charset="utf-8">
+    <style>
+      emphasis{
+        display:block;
+        border:1px solid red;
+        padding:10px;
+        margin: 10px 20px;
+      }
+    </style>
   </head>
   <body>
-    <?
-    print_r($bookInfo);
-    ?>
+    <img src="<? echo $coverImage?>">
   </body>
 </html>
